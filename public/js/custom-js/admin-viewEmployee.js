c2d8a5efbc7648f41dbd4e9a454c863b1e2286e9
Ogunsola <ogunsoladebayo@gmail.com>
2020-07-14 00:16:29 +0100

    revert changes

--- public/js/custom-js/admin-viewEmployee.js
+++ public/js/custom-js/admin-viewEmployee.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
-  const token = localStorage.getItem('tAuth');
+  const token = localStorage.getItem("tAuth");
   axios
-    .get('/v1/employee/all', {
+    .get("https://api.lancers.app/v1/employee/all", {
       headers: {
-        Authorization: 'Bearer ' + token,
+        Authorization: "Bearer " + token
         // "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluMkBnbWFpbC5jb20iLCJ1c2VySWQiOiIxOGEwYTk5Yy04MThkLTQ2YTYtOGJmYi1iMTdhYjczYWUyYTkiLCJ1c2VyUm9sZSI6IlJPTC1BRE1JTiIsInVzZXJUeXBlSWQiOm51bGwsImlhdCI6MTU5NDM5MjAxNiwiZXhwIjoxNTk0NDc4NDE2fQ.R5lajeNdXhiWiIKXm3MDhIJWfoJ2t7Mkhjb0BrnCzYQ"
-      },
+      }
     })
     .then(({ data }) => {
       let employees_count = [];
       let available_employees_count = [];
       let hired_employees_count = [];
 
-      data.data.all_employee_data.forEach((data) => {
+      data.data.all_employee_data.forEach(data => {
         employees_count.push(data);
-        if (data.avaliability.toLowerCase() == 'not-available') {
+        if (data.avaliability.toLowerCase() == "not-available") {
           hired_employees_count.push(data);
         }
-        if (data.avaliability.toLowerCase() == 'available') {
+        if (data.avaliability.toLowerCase() == "available") {
           available_employees_count.push(data);
         }
-        document.querySelector('#table-body-data').innerHTML += `
+        document.querySelector("#table-body-data").innerHTML += `
           <tr>
                                 <td class="align-middle user-id">${data.id}</td>
                                 <td class="align-middle user-name">
@@ -32,7 +32,7 @@ $(document).ready(function () {
                                 <td class="align-middle ui-table">UI/UX Designer</td>
                                 <td class="align-middle re-table">Remote</td>
                                 <td class="align-middle dt-table">${
-                                  data.createdAt.split('T')[0]
+                                  data.createdAt.split("T")[0]
                                 }</td>
                                 <td class="align-middle img-edit">
                                     <span class="stats-btn p-2 mr-md-3 available-txt rounded">${
@@ -52,11 +52,11 @@ $(document).ready(function () {
                             </tr>
           `;
       });
-      $('#all_employees').html(employees_count.length);
-      $('#hired_employees_count').html(hired_employees_count.length);
-      $('#available_employees_count').html(available_employees_count.length);
+      $("#all_employees").html(employees_count.length);
+      $("#hired_employees_count").html(hired_employees_count.length);
+      $("#available_employees_count").html(available_employees_count.length);
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       throw new Error(err);
     });
