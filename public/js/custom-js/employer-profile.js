8c6c389bb7c32d414cc5607b795c43090c9ba01c
Ogunsola <ogunsoladebayo@gmail.com>
2020-07-14 00:33:03 +0100

    revert changes

--- public/js/custom-js/employer-profile.js
+++ public/js/custom-js/employer-profile.js
@@ -1,7 +1,5 @@
-const {
-  getemployerdetails: [getemployerdetails],
-} = JSON.parse(localStorage.getItem('tpAuth'));
-const userInformation = JSON.parse(localStorage.getItem('tpAuth'));
+const { getemployerdetails: [getemployerdetails] } = JSON.parse(localStorage.getItem("tpAuth"));
+const userInformation = JSON.parse(localStorage.getItem("tpAuth"));
 const userInfo = JSON.parse(atob(userInformation.token.split('.')[1]));
 const pTags = document.querySelectorAll('p');
 const h3Tags = document.querySelectorAll('h3');
@@ -10,10 +8,10 @@ const spanTags = document.querySelectorAll('span');
 const h6Tags = document.querySelectorAll('h6');
 
 let [employerName] = h6Tags;
-let [, companyLogoSpan] = spanTags;
-let [employerImage, , companyLogo] = imgTags;
+let [ ,companyLogoSpan ,] = spanTags;
+let [ employerImage, ,companyLogo ,] = imgTags;
 let [companyName] = h3Tags;
-let [, companyCategory, companyLocation, email, , description] = pTags;
+let [ , companyCategory, companyLocation, email, , description ] = pTags;
 
 employerName.innerHTML = getemployerdetails.employer_name;
 employerImage.src = getemployerdetails.employer_photo;
@@ -26,40 +24,38 @@ email.innerHTML = getemployerdetails.employer_email;
 description.innerHTML = getemployerdetails.Company_category.description;
 
 const saveProfile = async () => {
-  const getEditProfileModalData = document.querySelectorAll('input');
-  let [
-    companyName,
-    companyTagLine,
-    companyEmail,
-    companyLocation,
-  ] = getEditProfileModalData;
-  let imageData = document.getElementById('inputGroupFile04');
-  [imageData] = imageData.files;
 
-  try {
-    const updateProfileUrl = `/v1/employer/update`;
+    const getEditProfileModalData = document.querySelectorAll('input');
+    let [companyName, companyTagLine, companyEmail, companyLocation] = getEditProfileModalData;
+    let imageData = document.getElementById('inputGroupFile04');
+    ([imageData] = imageData.files);
 
-    const fileUploadData = new FormData();
-    fileUploadData.append('photo', imageData);
-    fileUploadData.append('employer_name', companyName.value);
-    fileUploadData.append('employer_email', companyEmail.value);
-    fileUploadData.append('employer_address', companyLocation.value);
-    fileUploadData.append('employer_id', userInfo.userTypeId);
+    try {
 
-    const { data } = await axios({
-      method: 'PUT',
-      url: updateProfileUrl,
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-        Authorization: `Bearer ${userInformation.token}`,
-      },
-      data: fileUploadData,
-    });
+        const updateProfileUrl = `https://api.lancers.app/v1/employer/update`;
 
-    if (data.status === 'success') {
-      alert(data.message);
+        const fileUploadData = new FormData();
+        fileUploadData.append('photo', imageData);
+        fileUploadData.append('employer_name', companyName.value);
+        fileUploadData.append('employer_email', companyEmail.value);
+        fileUploadData.append('employer_address', companyLocation.value);
+        fileUploadData.append('employer_id', userInfo.userTypeId);
+
+        const { data } = await axios({
+            method: 'PUT',
+            url: updateProfileUrl,
+            headers: {
+              "Content-Type": "application/json; charset=UTF-8",
+              Authorization: `Bearer ${userInformation.token}`,
+            },
+            data: fileUploadData
+          });
+
+          if (data.status === 'success') {
+              alert(data.message);
+          }
+
+    } catch (e) {
+        console.log(e);
     }
-  } catch (e) {
-    console.log(e);
-  }
 };
