3e1434a095da61405abb84ffb718a398100a856a
OGUNSOLA USMAN ADEBAYO <57511444+ogunsoladebayo@users.noreply.github.com>
2020-07-14 07:39:10 +0100

    Delete support.js

--- Routes/support.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createSupport } = require('../controllers/support');
-
-const router = express.Router();
-router.post('/support', createSupport);
-
-module.exports = router;
